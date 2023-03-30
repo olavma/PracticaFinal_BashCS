b97d4538ff1ef3f928b4dd9510dce794c14fc445
olavma <olavma2004@gmail.com>
2023-03-30 21:04:42 +0200

    Ignorame.txt start

---
+++ Bash_To_CS/pracfinal/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace pracfinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            if (Directory.Exists(path)) Start(path);
+            else { Console.WriteLine("Como es posible que no tengas Escritorio?"); }
+        }
+
+        public static void Start(string path)
+        {
+            using (StreamWriter sw = File.AppendText(path + "\\ignorame.txt"))
+            {
+                sw.WriteLine("Buenos dias querido usuario. Este archivo es un texto que usted no deberia de estar mirando bajo ningun concepto");
+            }
+            //Console.WriteLine(file[8]);
+        }
+    }
+}
